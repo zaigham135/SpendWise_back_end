02ac6120ca89686cb62a41b2e64850414d123a8f
zaigham <zaigham135@gmail.com>
2025-07-21 00:15:24 +0530

    base_url_update

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key"; // Use environment variables
 const REFRESH_KEY = process.env.REFRESH_KEY || "your_refresh_secret_key";
 const BACKEND_PORT = process.env.PORT || 5000;
-const BASE_URL = `http://localhost:${BACKEND_PORT}`;
+// const BASE_URL = `http://localhost:${BACKEND_PORT}`;
+const BASE_URL = 'https://spendwise-back-end.onrender.com';
 
 module.exports = {
   SECRET_KEY,
